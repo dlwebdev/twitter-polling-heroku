c16240cdd9e15bf6bd262d49d72783eafc107078
Daniel Weber <dw@thedanielweber.com>
2016-08-19 15:54:39 +0000

    Updated twitter auth url

--- server/auth.js
+++ server/auth.js
@@ -5,6 +5,6 @@ module.exports = {
   'twitterAuth': {
     'consumerKey': 'GbXot4je0XFvQjwetqdjABHVM',
     'consumerSecret': 'v0ZK7uzEdx2JS7fdjcxP9oqaosluorpfYLERIvnCgC2aYzGNGE',
-    'callbackURL': 'https://polling-application-heroku-dlwebdev.c9users.io/auth/twitter/callback'
+    'callbackURL': 'https://twitter-polling-app.herokuapp.com/auth/twitter/callback'
   }    
 };
